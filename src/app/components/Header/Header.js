72f376535d610df8b2587261003fa327a203bf17
MdBillG <mdb8854@gmail.com>
2024-07-07 17:51:27 +0530

    working on mobile menu

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,13 +1,61 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import Navigation from '../Navigation/Navigation'
 import Content from '../Content/Content'
+import IconButton from '../buttons.js/iconButton';
+import MobileMenu from '../Mobile-Menu/MobileMenu';
 
 export default function Header() {
+
+    const [screenWidth, setScreenWidth] = useState(null);
+    const [mobileMenu, setMobileMenu] = useState(false)
+
+    useEffect(() => {
+        // Function to update screen width
+        const updateScreenWidth = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        // Initial screen width on component mount
+        updateScreenWidth();
+
+        // Event listener for screen width changes
+        window.addEventListener('resize', updateScreenWidth);
+
+        // Clean up the event listener
+        return () => {
+            window.removeEventListener('resize', updateScreenWidth);
+        };
+    }, []);
+
+    if (screenWidth === null) {
+        return null;
+    }
+
+    const showMobileMenu = () => {
+        setMobileMenu(true)
+    }
+
     return (
-        <div className=' flex justify-between px-12 pt-2 sm '>
-            <h1 className='text-2xl subpixel-antialiased tracking-wide font-medium text-[rgb(186,129,75)] '>Narikela</h1>
-            <Navigation />
+        <div className=' flex justify-between px-12 pt-2'>
+            {screenWidth >= 770 && (
+
+                <><h1 className='text-2xl subpixel-antialiased tracking-wide font-medium text-[rgb(186,129,75)] '>Narikela</h1><Navigation /></>
+            )}
+            {screenWidth <= 770 && (
+                <IconButton
+                    handleClick={showMobileMenu}
+                    icon=" fas fa-bars text-[rgb(186,129,75)]" />
+            )}
+
+            {mobileMenu && (
+                <MobileMenu />
+            )}
+
+
             <Content />
+
         </div>
     )
 }
--- src/app/components/Mobile-Menu/MobileMenu.js
+++ src/app/components/Mobile-Menu/MobileMenu.js
@@ -1,18 +1,15 @@
-"use client"
+import React from 'react'
 
-import React, { useState } from 'react';
-import IconButton from '../buttons.js/iconButton';
-
-export default function Navigation() {
+export default function MobileMenu() {
     return (
-        <>
-            < div className="flex gap-5" >
+        <div>
+            < div className="flex flex-col" >
                 <h1 className='text-xl subpixel-antialiased text-[rgb(186,129,75)]'>Home</h1>
                 <h1 className='text-xl subpixel-antialiased text-[rgb(186,129,75)]'>About</h1>
                 <h1 className='text-xl subpixel-antialiased text-[rgb(186,129,75)]'>Product</h1>
                 <h1 className='text-xl subpixel-antialiased text-[rgb(186,129,75)]'>Testimonials</h1>
                 <h1 className='text-xl subpixel-antialiased text-[rgb(186,129,75)]'>Contact</h1>
             </div>
-        </ >
-    );
+        </div>
+    )
 }
