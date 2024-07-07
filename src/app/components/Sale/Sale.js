1a36f1af051972a2d13437365c59643b0f74ac98
MdBillG <mdb8854@gmail.com>
2024-07-07 19:19:20 +0530

    added marquree tag

--- src/app/components/Sale/Sale.js
+++ src/app/components/Sale/Sale.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Sale() {
     return (
-        <div className='bg-[rgb(186,129,75)] shadow-xl '>
+        <div className='bg-[rgb(186,129,75)] shadow-xl h-10 '>
 
             <marquee direction="right" behavior="alternate" className="">
-                <div className='text-cyan-100 pt-1'>Free Shipping on orders over Rs.999/-</div>
+                <div className='text-cyan-100 pt-2'>Free Shipping on orders over Rs.999/-</div>
             </marquee>
 
         </div>
