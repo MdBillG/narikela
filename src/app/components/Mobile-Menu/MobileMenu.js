10a160af2ca0bd855c9370669ab55f30792c3570
MdBillG <mdb8854@gmail.com>
2024-07-30 12:41:39 +0530

    working on side bar

--- src/app/components/Mobile-Menu/MobileMenu.js
+++ src/app/components/Mobile-Menu/MobileMenu.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import IconButton from '../buttons.js/iconButton'
 
-export default function MobileMenu({ setMobileMenu }) {
+const MobileMenu = ({ setMobileMenu }) => {
+    const NavItem = ({ href, children, expandable }) => (
+        <a
+            href={href}
+            className="py-3 px-4 border-b border-[#5d7855] flex justify-between items-center text-white"
+        >
+            {children}
+            {expandable && <span className="text-sm">▼</span>}
+        </a>
+    );
 
     return (
-        <div className=" bg-white  top-0 left-0  z-20 p-2 rounded-lg h-screen w-screen absolute opacity-95">
-            <div className="flex flex-col   text-xl subpixel-antialiased text-pri rounded-lg ">
-
-                <a href='/about'>About Us</a>
-                <a >Products</a>
-                <a href='/blogs'>Blogs</a>
-                <a href='/contact'>Contact Us</a>
+        <div className="bg-[#4C6444] top-0 left-0 z-20 h-screen w-[70%] absolute">
+            <div className='py-4 px-4 bg-[#DCD1BF] flex justify-between items-center'>
+                <span className="text-[#4C6444] text-xl font-bold">Narikela</span>
+                <button
+                    onClick={() => setMobileMenu(false)}
+                    className="text-[#4C6444] text-xl"
+                >
+                    ✕
+                </button>
             </div>
-            <div className='absolute top-5 right-5'>
-                <IconButton
-                    handleClick={() => setMobileMenu(false)}
 
-                    icon=" fas fa-times text-[rgb(186,129,75)] fa-xl" />
-
+            <div className="flex flex-col text-lg">
+                <NavItem href='/'>Home</NavItem>
+                <NavItem href='/about'>About Us</NavItem>
+                <NavItem href='/plp' >Products</NavItem>
+                <NavItem href='/blogs' >Blogs</NavItem>
+                <NavItem href='/contact'>Contact Us</NavItem>
             </div>
         </div>
-    )
-}
+    );
+};
+
+export default MobileMenu;
